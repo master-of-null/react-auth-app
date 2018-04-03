dc0ff618939e50674bf01cbe6a7a610eb958936d
Gabo Martinez <gabmart02@gmail.com>
2018-04-03 12:13:15 -0400

    ADDS Login Form to main app page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp(
+			{
+				apiKey: 'AIzaSyDqqiM4Lwh9TQTMBrjLdeWIBSUgQtYoyJM',
+				authDomain: 'firstreactauthproject.firebaseapp.com',
+				databaseURL: 'https://firstreactauthproject.firebaseio.com',
+				projectId: 'firstreactauthproject',
+				storageBucket: 'firstreactauthproject.appspot.com',
+				messagingSenderId: '945444488791'
+			}
+		);
+
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An app</Text>
+				<LoginForm />
 			</View>
 		);
 	}

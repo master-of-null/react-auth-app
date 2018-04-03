3ab8b59e13a247937d213389c0ee5095253a3649
Gabo Martinez <gabmart02@gmail.com>
2018-04-03 11:29:38 -0400

    ADDS header to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
 	render() {
 		return (
 			<View>
+				<Header headerText="Authentication" />
 				<Text>An app</Text>
 			</View>
 		);
@@ -12,4 +14,3 @@ class App extends Component {
 };
 
 export default App;
-
